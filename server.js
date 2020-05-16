f2c0360112497dcecda9ebdfb57acf6429b621f8
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2020-05-16 19:24:29 -0400

    bootstrap issue

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static(__dirname + '/public'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 // ================================================================================
 // ROUTER
 // ================================================================================
